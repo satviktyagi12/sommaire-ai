fbad6d63f899375c28f4be7450c0972012cc344b
satviktyagi12 <satviktyagi41@gmail.com>
2026-01-13 22:14:31 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Source_Sans_3 as FontSans } from "next/font/google";
+import "./globals.css";
+
+const fontSans = FontSans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+
+
+export const metadata: Metadata = {
+  title: "sommaire",
+  description: "an app for summarising PDF documents",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${fontSans.variable} font-sans antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
