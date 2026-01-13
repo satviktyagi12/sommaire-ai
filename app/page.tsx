806a592af1456580bffcc1553c5c4c5302e68ffb
satviktyagi12 <satviktyagi41@gmail.com>
2026-01-13 22:02:08 +0530

    Project setup and added Shadcn

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="outline" size={'lg'}>Shadcn Button</Button>
+    </div>
+  );
+}
